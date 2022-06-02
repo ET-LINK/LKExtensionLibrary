1a771a29de29055bad4681a54a09b1f5de3769df
ET-LINK <ke.liful@gmail.com>
2022-06-02 11:57:24 +0800

    add: 添加codable 扩展

---
+++ Sources/LKLibrary/Extension/Foundation/Codable+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//
+//
+//  Created by Enter on 2022/6/2.
+//
+
+import Foundation
+
+public extension Encodable {
+    var dictionary: [String: Any]? {
+        guard let data = try? JSONEncoder().encode(self) else { return nil }
+        return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] }
+    }
+}
